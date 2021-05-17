65f95f94c3f786911dd8d5a1f0bcad4c13eba803
Gabor Szilagyi <szilagyi_gabor@outlook.com>
2021-05-17 13:30:04 +0200

    BCKLG-24: wired up i18n package

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,16 @@
 import logoPlaceholder from '../../assets/images/logo-placeholder.png';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const { t } = useTranslation();
   return (
     <div className="navbar">
       <img src={logoPlaceholder} alt="logo placeholder" />
       <ul className="navbar-list">
         <Link to='/'>
-          <li className="navbar-list-item">Főoldal</li>
+          <li className="navbar-list-item">{t('homepage')}</li>
         </Link>
         <Link to="/jatekok">
           <li className="navbar-list-item">Játékok</li>
